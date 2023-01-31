e5de547e9b667f25d8eccc8e713cbb0c8efcdf55
Gabriela Saenz <123127101+gabrisae@users.noreply.github.com>
2023-01-30 22:08:54 -0700

    Smallest Number

---
+++ Homework2 Part2.cs
@@ -0,0 +1,9 @@
+﻿namespace Homework2Smallestvalue;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int number = Convert.ToInt16(Console.Readline();)
+        Console.WriteLine("")
+    }
+}
