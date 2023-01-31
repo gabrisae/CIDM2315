7b256f58a4a9dfd90f2e907bd479e25b15ee9e52
Gabriela Saenz <123127101+gabrisae@users.noreply.github.com>
2023-01-30 21:39:40 -0700

    GPA Points

---
+++ Homework2 Part1.cs
@@ -0,0 +1,25 @@
+﻿namespace Homework2;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Please input a letter  grade:");
+      //var A = 4, B = 3, C = 2, D = 1, F = 0;
+       string letter = Console.ReadLine();
+        if(letter == "A"){
+            Console.WriteLine("GPA point: 4");
+        }else if(letter == "B"){
+            Console.WriteLine("GPA point: 3");
+        }else if(letter == "C"){
+            Console.WriteLine("GPA point: 2");
+        }else if(letter == "D"){
+            Console.WriteLine("GPA point: 1");
+        }else if(letter == "F"){
+            Console.WriteLine("GPA point: 0");
+        }else{
+            Console.WriteLine("Wrong Letter Grade!");
+        }
+        
+
+   }
+}
