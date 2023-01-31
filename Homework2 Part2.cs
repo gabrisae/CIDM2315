4c175b45877ce4d1c30b007d321d5936b4d7c8ae
Gabriela Saenz <123127101+gabrisae@users.noreply.github.com>
2023-01-30 23:46:57 -0700

    Update Homework2 Part2.cs
    I could not find what I was doing wrong.

--- Homework2 Part2.cs
+++ Homework2 Part2.cs
@@ -3,7 +3,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        int number = Convert.ToInt16(Console.Readline();)
-        Console.WriteLine("")
-    }
-}
+       
+       int num1, num2, num3;
+       Console.WriteLine("Please input the first num:");
+       int num1 = Convert.ToInt16(Console.ReadLine());
+       Console.WriteLine("Please input the second num:");
+       int num2 = Convert.ToInt16(Console.ReadLine());
+       Console.WriteLine("Please input the third num:");
+       int num3 = Convert.ToInt16(Console.ReadLine());
+
+        if(n1 < n2){
+            if(n1 < n3){
+                Console.WriteLine("The smallest value is: n1");
+            }
+            else{
+                Console.WriteLine("The smallest value is: n3");
+            }
+        }
+        else{
+            if(n2 < n3){
+                Console.WriteLine("The smallest value is: n2");
+            }
+            else{
+                Console.WriteLine("The smallest value is: n3");
+            }
+        }
+
+
+
+        }
+
+
+
+}       
