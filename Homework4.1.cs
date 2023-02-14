e1ba595f132adc0fd7e010e582f809d8ceee3b2c
Gabriela Saenz <123127101+gabrisae@users.noreply.github.com>
2023-02-13 21:27:07 -0700

    Homework 4 part 1

---
+++ Homework4.1.cs
@@ -0,0 +1,14 @@
+﻿namespace Homework4;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("\nInput first integer:");  
+        int a = Convert.ToInt16(Console.ReadLine());
+        Console.WriteLine("Input second integer:");  
+        int b = Convert.ToInt16(Console.ReadLine());
+
+        Console.WriteLine("a = " + a + ";" + " b = " +b);
+        Console.WriteLine("The largest number is: "+(Math.Max(a, b)));
+    }
+}
