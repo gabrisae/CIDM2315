4df59ae0ce31fa786be9379e713d257e352c1e8c
Gabriela Saenz <123127101+gabrisae@users.noreply.github.com>
2023-02-13 22:12:53 -0700

    Homework 4
    The second part of the assignment needs to be taken out of /*  */.
    I'm not exactly sure how to turn it in with both codes or how you prefer it since you asked we only submit one code file.
    I looked over the lecture and its not clear on how to do it when the homework asks for more than one code.

--- Homework4.cs
+++ Homework4.cs
@@ -2,6 +2,7 @@
 class Program
 {
     static void Main(string[] args)
+    
     {
         Console.WriteLine("\nInput first integer:");  
         int a = Convert.ToInt16(Console.ReadLine());
@@ -12,3 +13,29 @@ class Program
         Console.WriteLine("The largest number is: "+(Math.Max(a, b)));
     }
 }
+
+
+/*
+namespace Homework4._2;
+class Program
+{
+    
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("N is: 5; shape is left");
+        for(int i=1; i <= 5; i++)
+    {
+        for(int j = 1; j <= i; j++)
+        {
+            
+           Console.Write("*");
+        }
+
+        Console.WriteLine();
+        }
+            Console.Read();
+        
+        }
+    }
+*/
