75742fc5f14af3ca4827116b657d792af3ccc115
Gabriela Saenz <123127101+gabrisae@users.noreply.github.com>
2023-02-20 22:24:22 -0700

    Homework 5
    Not my best work, I kept getting confused.

---
+++ Homework5.cs
@@ -0,0 +1,79 @@
+﻿namespace Homework5;
+class Program
+{
+     
+       static void Main(string[] args)
+    {
+        Console.WriteLine("\nInput first integer:");  
+        int a = Convert.ToInt16(Console.ReadLine());
+        Console.WriteLine("Input second integer:");  
+        int b = Convert.ToInt16(Console.ReadLine());
+
+        Console.WriteLine("a = " + a + ";" + " b = " +b);
+        Console.WriteLine("The largest number is: "+(Math.Max(a, b)));
+        
+    }
+}
+
+
+    /*
+    static void Main(string[] args)
+    {
+        Console.WriteLine("\nInput first integer:");  
+        int a = Convert.ToInt16(Console.ReadLine());
+        Console.WriteLine("Input second integer:");  
+        int b = Convert.ToInt16(Console.ReadLine());
+        Console.WriteLine("Input third integer:");  
+        int c = Convert.ToInt16(Console.ReadLine());
+        Console.WriteLine("Input fourth integer:");  
+        int d = Convert.ToInt16(Console.ReadLine());
+
+        
+        
+        Console.Write("a = " + a + ";" + " b = " + b);
+        Console.WriteLine("; c = " + c + ";" + " d = " + d);
+        Console.WriteLine("The largest number is: " +Math.Max(a, Math.Max(b, Math.Max(c, d))));
+        
+    }
+}
+*/
+
+
+
+
+
+
+ /*   static bool checkAge(int birth_year) {
+  
+         birth_year(age = 2022-birthyear);
+
+            if
+            (age < 18) {
+              return true; 
+
+            } else {
+              return false; 
+         }
+        }
+     } 
+    }
+
+
+
+
+Console.Write("Enter Your UserName: ");
+var userName = Console.ReadLine();
+Console.Write("Enter Your Password: ");
+var password = Console.ReadLine();
+Console.Write("Enter Your Password Again: ");
+var passwordAgain = Console.ReadLine();
+Console.Write("Enter Your Birthyear: ");
+var birthYear = Console.ReadLine();
+
+
+if( userName == "alice" && password == "alice")
+   
+    }
+}
+
+*/
